dc5227e826711e972f27319465045767e0f58a5e
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-06-07 18:07:46 +0300

    projects created

--- src/components/Section/Projects/Projects.jsx
+++ src/components/Section/Projects/Projects.jsx
@@ -1,15 +1,33 @@
+
+import { AiOutlineArrowRight } from 'react-icons/ai';
 import { useScrollContext } from "../../../Contexts/scrollContext";
 import data from "../../../JSON/projects.json"
 import "./projects.css";
+import { useEffect, useState } from 'react';
 
 
 function Projects() {
   const {sectionRefs} = useScrollContext();
+  const [hoveredID , setHoveredID] = useState(null)
+  const [isHovered , setIsHovered] = useState(false)
+
+  const mouseOverHandle = (id) => {
+      setHoveredID(id)
+  }
+  const mouseOutHandle = () => {
+    setHoveredID(null)
+    setIsHovered(false)
+  }
+  
+  useEffect(() => {
+    setIsHovered(true)
+  },[hoveredID])
+
   return (
     <section ref={sectionRefs.projects}>
       <h1>Projects</h1>
         {
-          data.map((item) => {
+          data.map((item) => { 
             return (
               <div className="project-card" key={item.id}>
                 <div className="card-body">
@@ -24,7 +42,20 @@ function Projects() {
                     {item.description}
                   </div>
                   <div className="card-buttons">
-                    <a href="#">Visit on GitHub...</a>
+                    <button 
+                    className='btn' 
+                    onMouseOver={() =>mouseOverHandle(item.id) }
+                    onMouseLeave={mouseOutHandle}
+                    >
+                      <a href={item.link} target="_blank" rel="noreferrer" >
+                      <AiOutlineArrowRight />
+                      </a>
+                    </button >
+                      <span className={`link ${item.id===hoveredID&&isHovered&& "d-block"}`}> 
+                        <a>
+                          Visit GitHub...
+                        </a>
+                      </span>
                   </div>
                 </div>
                   <div className={item.id % 2 === 0 ? "d-none" : ""}>

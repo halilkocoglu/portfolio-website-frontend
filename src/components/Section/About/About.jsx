bf33f74637bd99db63983dac91e0b5a1bad2b306
Halil Kocoglu <halilkocoglu98@gmail.com>
2023-06-08 22:19:31 +0300

    Cv download added.

--- src/components/Section/About/About.jsx
+++ src/components/Section/About/About.jsx
@@ -1,30 +1,35 @@
 import './about.css';
 import { useScrollContext } from '../../../Contexts/scrollContext';
+import  {MdDownload}  from 'react-icons/md';
 
 function About() {
   const {sectionRefs} = useScrollContext()
   return (
     <section ref={sectionRefs.about} >
-      <div className='div'>
-        <h1>About</h1>
+      <div>
+        <div className='about-info'>
+          <h1>About</h1>
           <p>
-          <span className='intended' >
-            I graduated from Necmettin Erbakan University Aviation Management.</span>
+            <span className='intended' ></span>
+            I graduated from Necmettin Erbakan University Aviation Management.
             Since 2018, I have combined my interest in the computer and software industry with determination. 
-          
           </p>
           <p>
-          <span className='intended'>
-            After September 2022, I have devoted myself to training and developing on the software industry(especially web development). </span>
+            <span className='intended'></span>
+            After September 2022, I have devoted myself to training and developing on the software industry(especially web development). 
             I want to be MERN Stack Developer at first. After that I want to go for  Full-Stack Web Development. I currently live in Aydın, however I can move for work if I have to.
-          
           </p>
           <p>
-          <span className='intended'>
-            I love reading mythological stories and books,</span> fun-sounding rhythms and musics, pets, playing (story-based) computer games. Although I am prone to group work, mostly I prefer to have fun with myself in my private and social life.
-          
+            <span className='intended'></span>
+            I love reading mythological stories and books, fun-sounding rhythms and musics, pets, playing (story-based) computer games. Although I am prone to group work, mostly I prefer to have fun with myself in my private and social life.
           </p>
-          </div>
+        </div>
+        <div className='cv'>
+          <button>
+          <a href="" download>CV <MdDownload /></a>
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
